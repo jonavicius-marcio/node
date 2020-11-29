748f1ce201d3bb8ee719ed121085cf2571b14659
Marcio <56298992+jonavicius-marcio@users.noreply.github.com>
2020-11-29 15:52:54 +0000

    Update mysql.js

--- API/mysql.js
+++ API/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 var pool = mysql.createPool({
     "connectionLimit" : 1000,
     "user" :"root",
-    "password": "mark159",
+    "password": "###",
     "database" : "mydb",
     "host": "localhost",
     "port" : 3306
